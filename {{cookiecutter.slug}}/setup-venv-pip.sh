b5636de11273e6798069db6cdb964642687f2a71
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2023-12-01 13:32:45 +0100

    Update for reprepbuild-1.7.1

--- {{cookiecutter.slug}}/setup-venv-pip.sh
+++ {{cookiecutter.slug}}/setup-venv-pip.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-# This script assumes you have a running and somewhat modern Python environment.
 
+# This script assumes you have a running and somewhat modern Python environment.
 PYTHON3=python3
+${PYTHON3} -c 'import sys; assert sys.version_info.major == 3; assert sys.version_info.minor >= 11'
 
 echo "Create the venv"
 ${PYTHON3} -m venv venv
