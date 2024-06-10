f98ea732e82dce60fa623bdac6efde49299891f4
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2024-06-10 10:25:32 +0200

    Document how to set up with non-default Python

--- {{cookiecutter.slug}}/setup-venv-pip.sh
+++ {{cookiecutter.slug}}/setup-venv-pip.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-# This script assumes you have a running and somewhat modern Python environment.
+# If you want to install with a Python version that is not your OS's default:
+# PYTHON3=/usr/bin/python3.11 ./setup-venv-pip.sh
 : "${PYTHON3:=$(which python3)}"
+
+# This script assumes you have a running and somewhat modern Python environment.
 ${PYTHON3} -c 'import sys; assert sys.version_info.major == 3; assert sys.version_info.minor >= 11'
 
 echo "Create the venv"
