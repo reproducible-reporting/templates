17e7beedff4f4c327cfc393398ed3a1e4bed1d1e
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-05-12 14:35:09 +0200

    StepUp 3 updates

--- {{cookiecutter.slug}}/archives/create_archives.sh
+++ {{cookiecutter.slug}}/archives/create_archives.sh
@@ -7,15 +7,15 @@ cd ..
 
 # Execute all plans non-interactively in alphabetical order.
 # (Make sure this is also the chronological order.)
-for d in */; do (cd $d && if [[ -f plan.py ]]; then stepup; fi); done
+for d in */; do (cd $d && if [[ -f plan.py ]]; then stepup boot; fi); done
 
 # Create the bundle with the source history and store it in archives.
 git bundle create archives/main.bundle main
 
 # Create an inventory and a reproducible zip file with outputs
-rr-make-inventory -i main-inventory.def -o main-inventory.txt
-rr-zip-inventory main-inventory.txt archives/main-latest-with-outputs.zip
+stepup make-inventory -i main-inventory.def -o main-inventory.txt
+stepup zip-inventory main-inventory.txt archives/main-latest-with-outputs.zip
 
 # Make a final inventory file of the archived data
 cd archives
-rr-make-inventory -o main-archive-inventory.txt README.md main.bundle main-latest-with-outputs.zip
+stepup make-inventory -o main-archive-inventory.txt README.md main.bundle main-latest-with-outputs.zip
